8c4f6d7629f23dc4e958e93beefe155a7dcf1f95
Tony <tony.ding@insight.com>
2020-08-19 18:28:45 +0800

    分享

--- wp-content/themes/twentytwenty/single-scfq.php
+++ wp-content/themes/twentytwenty/single-scfq.php
@@ -4,9 +4,10 @@ get_header();
 <main id="site-content" role="main">
 	<div class="detail container scfq">
         <div class="inside">
-            <div class="postcontent">
+            <div class="postcontent shares">
 				<p class="t"><?php echo $post->post_title; ?></p>
-				<p class="date">发表于：<?php echo get_the_date('Y-m-d'); ?></p>
+				<div class="date"><?php echo get_the_date('Y年m月d日 H时m分'); ?>　<?php if($post->post_excerpt)echo '来源：'.$post->post_excerpt; ?>
+				<div class="bshare-custom icon-medium" style="float: right;"><a title="更多平台" class="bshare-more bshare-more-icon more-style-addthis"></a><a title="分享到微信" class="bshare-weixin"></a><a title="分享到新浪微博" class="bshare-sinaminiblog"></a></div><script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/buttonLite.js#style=-1&amp;uuid=&amp;pophcol=2&amp;lang=zh"></script><script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/bshareC0.js"></script><div class="clear"></div></div>
 				<div class="rc">
 				<img src="<?php $full_image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full');echo $full_image_url[0]; ?>" alt="">
 					<?php echo $post->post_content; ?>
--- wp-content/themes/twentytwenty/single-window.php
+++ wp-content/themes/twentytwenty/single-window.php
@@ -4,9 +4,10 @@ get_header();
 <main id="site-content" role="main">
 	<div class="detail container">
         <div class="inside nocategory">
-            <div class="postcontent">
+            <div class="postcontent shares">
 				<p class="t"><?php echo $post->post_title; ?></p>
-				<p class="date">发表于：<?php echo get_the_date('Y-m-d'); ?></p>
+				<div class="date"><?php echo get_the_date('Y年m月d日 H时m分'); ?>　<?php if($post->post_excerpt)echo '来源：'.$post->post_excerpt; ?>
+				<div class="bshare-custom icon-medium" style="float: right;"><a title="更多平台" class="bshare-more bshare-more-icon more-style-addthis"></a><a title="分享到微信" class="bshare-weixin"></a><a title="分享到新浪微博" class="bshare-sinaminiblog"></a></div><script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/buttonLite.js#style=-1&amp;uuid=&amp;pophcol=2&amp;lang=zh"></script><script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/bshareC0.js"></script><div class="clear"></div></div>
 				<div class="rc">
 					<?php echo $post->post_content; ?>
 				</div>
