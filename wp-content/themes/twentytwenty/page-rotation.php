b9b4949b277a16a9807e7065985e7d58de581365
dingyatao123 <992776686@qq.com>
2020-09-08 19:42:37 +0800

    轮播图

--- wp-content/themes/twentytwenty/page-rotation.php
+++ wp-content/themes/twentytwenty/page-rotation.php
@@ -10,6 +10,7 @@ get_header();
 				<div class="bshare-custom icon-medium" style="float: right;"><a title="更多平台" class="bshare-more bshare-more-icon more-style-addthis"></a><a title="分享到微信" class="bshare-weixin"></a><a title="分享到新浪微博" class="bshare-sinaminiblog"></a></div><script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/buttonLite.js#style=-1&amp;uuid=&amp;pophcol=2&amp;lang=zh"></script><script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/bshareC0.js"></script><div class="clear"></div></div>
 				<div class="rc">
 					<img width="100%" src="<?php $full_image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full');echo $full_image_url[0]; ?>" alt="">
+					<br>
 					<?php echo $post->post_content; ?>
 				</div>
             </div>
